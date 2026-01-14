cec95d3bac40300f173fa2be61c7bd6cbae43294
martin4art <167979580+martin4art@users.noreply.github.com>
2026-01-14 13:05:58 +0100

    Add Jest config for backend tests

---
+++ backend/jest.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  moduleFileExtensions: ['js', 'json', 'ts'],
+  rootDir: '.',
+  testEnvironment: 'node',
+  testRegex: '.*\\.spec\\.ts$',
+  transform: {
+    '^.+\\.(t|j)s$': ['ts-jest', { tsconfig: 'tsconfig.json' }],
+  },
+  coverageDirectory: '../coverage',
+  collectCoverageFrom: ['src/**/*.ts'],
+};
